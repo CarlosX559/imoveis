5cb5b8aeee02fe35cd1e94fb10f66e7c6c1d027a
Carlos Xavier <carlos.lipe559@hotmail.com>
2022-11-18 17:26:38 -0300

    enviando arquivos

---
+++ js/script.js
@@ -0,0 +1,60 @@
+function modal() {
+
+    var modal = document.querySelectorAll('.img');
+    var closeModal = document.querySelector('.area_modal_int svg ');
+    modal.forEach((element) => {
+        element.addEventListener('click', () => {
+
+            $('.area_modal').css('display', 'flex').css('opacity', 1);
+            var imgAtual = element.getAttribute('data-img');
+            var title = element.getAttribute('data-title');
+            
+            document.querySelector('.modal img').setAttribute('src', imgAtual);
+            document.querySelector('.texts_modal h2').innerHTML = title;
+
+        });
+
+    });
+    closeModal.addEventListener('click', () => {
+        $('.area_modal').css('opacity', 0);
+        setTimeout(() => {
+            $('.area_modal').css('display', 'none')
+
+        }, 300);
+
+    });
+
+}
+modal();
+
+function menu(){
+    $(".menu_opener").click(
+        function(){
+            
+            $('.area_menu_mobile_total').css( 'display', 'flex' ).css( 'top', '100px' ).css( 'animation', 'move 1s ease' );
+            setTimeout( ()=> {
+                $('.menu_opener').css( 'display', 'none' );
+                $('.menu_close').css( 'display', 'flex' );
+            }, 600 );
+        }  
+    );
+
+    $(".menu_close").click(
+        function() {
+            $('.area_menu_mobile_total').css( 'top', '-100%' ).css('animation', 'move 1s ease' );
+            setTimeout( () => {
+                if($('.area_menu_mobile_total').css('top', '-100%')) {
+                    $('.area_menu_mobile_total').css('display', 'none');
+                }
+            }, 600);
+
+            setTimeout( ()=> {
+                $('.menu_opener').css( 'display', 'flex' );
+                $('.menu_close').css( 'display', 'none' );
+            }, 100 );
+            
+        }
+        
+    );
+}
+menu();

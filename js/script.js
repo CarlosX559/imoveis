f5762eb98775b3b65e75fbac005108b3644f9990
Carlos Xavier <carlos.lipe559@hotmail.com>
2022-11-19 14:34:01 -0300

    responsive iphone flex

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function modal() {
     closeModal.addEventListener('click', () => {
         $('.area_modal').css('opacity', 0);
         setTimeout(() => {
-            $('.area_modal').css('display', 'none')
+            $('.area_modal').css('display', 'none', 'important')
 
         }, 300);
 
@@ -31,10 +31,10 @@ function menu(){
     $(".menu_opener").click(
         function(){
             
-            $('.area_menu_mobile_total').css( 'display', 'flex' ).css( 'top', '100px' ).css( 'animation', 'move 1s ease' );
+            $('.area_menu_mobile_total').css( 'display', '-webkit-flex' ).css( 'top', '100px' ).css( 'animation', 'move 1s ease' );
             setTimeout( ()=> {
-                $('.menu_opener').css( 'display', 'none' );
-                $('.menu_close').css( 'display', 'flex' );
+                $('.menu_opener').css( 'display', 'none', 'important' );
+                $('.menu_close').css( 'display', '-webkit-flex' );
             }, 600 );
         }  
     );
@@ -44,13 +44,13 @@ function menu(){
             $('.area_menu_mobile_total').css( 'top', '-100%' ).css('animation', 'move 1s ease' );
             setTimeout( () => {
                 if($('.area_menu_mobile_total').css('top', '-100%')) {
-                    $('.area_menu_mobile_total').css('display', 'none');
+                    $('.area_menu_mobile_total').css('-webkit-flex', 'none', 'important');
                 }
             }, 600);
 
             setTimeout( ()=> {
-                $('.menu_opener').css( 'display', 'flex' );
-                $('.menu_close').css( 'display', 'none' );
+                $('.menu_opener').css( 'display', '-webkit-flex' );
+                $('.menu_close').css( 'display', 'none', 'important' );
             }, 100 );
             
         }
